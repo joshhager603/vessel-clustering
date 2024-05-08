1c5ec692a4089adf921310f94d2da2250763d357
Josh Hager <joshhager@dhcp-10-5-141-10.wireless.rochester.edu>
2024-05-07 21:21:57 -0400

    basic setup for kmeans

--- 2_predictVessel.py
+++ 2_predictVessel.py
@@ -18,7 +18,7 @@ def predictor_baseline(csv_path):
     # Standardization 
     X = preprocessing.StandardScaler().fit(X).transform(X)
     # k-means with K = number of unique VIDs of set1
-    K = 20 
+    K = 20
     model = KMeans(n_clusters=K, random_state=123, n_init='auto').fit(X)
     # predict cluster numbers of each sample
     labels_pred = model.predict(X)
@@ -43,13 +43,52 @@ def evaluate():
     print(f'Adjusted Rand Index Score of set3.csv: {rand_index_score:.4f}')
 
 
-def predictor(csv_path):
-    # fill your code here
+def baseline_preprocess(csv_path):
+    # load data and convert hh:mm:ss to seconds
+    df = pd.read_csv(csv_path, converters={'SEQUENCE_DTTM' : hh_mm_ss2seconds})
+    # select features 
+    selected_features = ['SEQUENCE_DTTM', 'LAT', 'LON', 'SPEED_OVER_GROUND' ,'COURSE_OVER_GROUND']
+    X = df[selected_features].to_numpy()
+    # Standardization 
+    X = preprocessing.StandardScaler().fit(X).transform(X)
+
+    return X
+
+def k_means_preprocess(X):
+    return X
+
+def k_means_train(X):
+    return X
+
+def k_means_predictor(csv_path):
+    X = baseline_preprocess(csv_path)
+
+    X = k_means_preprocess(X)
+
+    # change this to the optimal hyperparameters found through training
+    model = KMeans(n_clusters=20, random_state=123, n_init='auto').fit(X)
+
+    labels_pred = model.predict(X)
+
     return labels_pred
 
+def evaluate_general(csv_path, predictor):
+    labels_true = pd.read_csv(csv_path)['VID'].to_numpy()
+    labels_pred = predictor(csv_path)
+    rand_index_score = adjusted_rand_score(labels_true, labels_pred)
+    print(f'Adjusted Rand Index Score of {csv_path}: {rand_index_score:.4f}')
 
-if __name__=="__main__":
-    get_baseline_score()
-    evaluate()
+def predictor(csv_path):
+    # fill your code here
+    #return labels_pred
+    pass
+
+get_baseline_score()
+evaluate_general('./Data/set1.csv', k_means_predictor)
+evaluate_general('./Data/set2.csv', k_means_predictor)
+
+# if __name__=="__main__":
+#     get_baseline_score()
+#     evaluate()
 
 
